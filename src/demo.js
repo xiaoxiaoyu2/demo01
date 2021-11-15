71bcdb38903e955e13a9f945e1e981282c72d857
xiaoxiaoyu2 <1406593871@qq.com>
2021-11-15 14:00:05 +0800

    https://github.com/xiaoxiaoyu2/demo01/blob/first-commit/src/demo.js

--- src/demo.js
+++ src/demo.js
@@ -239,12 +239,12 @@
 
 
 
-// import promise, { output } from "./awaiting.js";
-// function outputPlusValue(value) { return output + value };
-// promise.then(() => {
-//     console.log(outputPlusValue(100));
-//     setTimeout(() => console.log(outputPlusValue(100)), 1000);
-// });
+import promise, { output } from "./awaiting.js";
+function outputPlusValue(value) { return output + value };
+promise.then(() => {
+    console.log(outputPlusValue(100));
+    setTimeout(() => console.log(outputPlusValue(100)), 1000);
+});
 
 
 
